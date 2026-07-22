ef382afc84d55ea75959e4e5fc5de3a81c636d8a
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-07-22 16:09:52 -0400

    fix

--- oxiquizkit/benches/benchmarks.rs
+++ oxiquizkit/benches/benchmarks.rs
@@ -1,11 +1,53 @@
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use std::hint::black_box;
+use ndarray::Array2;
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use oxiquizkit::holography::HolographyEngine;
 use oxiquizkit::fibonacci::{fibonacci_slow, fibonacci_fast};
 
-pub fn benchmark_fibonacci(c: &mut Criterion) {
+fn benchmark_fibonacci(c: &mut Criterion) {
     c.bench_function("fib(20)", |b| b.iter(|| fibonacci_fast(black_box(20))));
 }
 
+fn benchmark_gerchberg_saxton(c: &mut Criterion) {
+    let size: usize = 512;
+    let iterations = 25;
+
+    let mut group = c.benchmark_group("Gerchberg-Saxton");
+
+    // TODO 
+    group.sample_size(10); 
+
+    // TODO
+    let mut target_amplitude = Array2::<f64>::zeros((size, size));
+
+    let center = size / 2;
+    let spacing = 20;
+
+    target_amplitude[[center - spacing, center - spacing]] = 1.0;
+    target_amplitude[[center + spacing, center - spacing]] = 1.0;
+    target_amplitude[[center - spacing, center + spacing]] = 1.0;
+    target_amplitude[[center + spacing, center + spacing]] = 1.0;
+
+    let slm_illumination = Array2::<f64>::from_elem((size, size), 1.0);
+    let mut engine = HolographyEngine::new(size, size);
+
+    group.bench_with_input(
+        BenchmarkId::new("GS_512x512", iterations), 
+        &iterations, 
+        |b, &iters| {
+            b.iter(|| {
+                engine.gerchberg_saxton_slm_phase(
+                    &target_amplitude, 
+                    &slm_illumination, 
+                    iters
+                )
+            })
+        }
+    );
+
+    group.finish();
+}
+
 // NB cargo bench -- --fail-fast 
-criterion_group!(benches, benchmark_fibonacci);
+criterion_group!(benches, benchmark_gerchberg_saxton);
 criterion_main!(benches);
