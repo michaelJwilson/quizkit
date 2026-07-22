67a5ec0986cedd88201e095fd8a36b5dd5ed6b2b
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-07-22 16:17:55 -0400

    fix

--- oxiquizkit/src/holography.rs
+++ oxiquizkit/src/holography.rs
@@ -19,58 +19,48 @@ pub struct HolographyEngine {
 impl HolographyEngine {
     pub fn new(rows: usize, cols: usize) -> Self {
         let n_elements = rows * cols;
+        
         let mut buf_a = AlignedVec::<Complex64>::new(n_elements);
         let mut buf_b = AlignedVec::<Complex64>::new(n_elements);
 
-        // NB forward: slm -> trap
+        for i in 0..n_elements {
+            buf_a[i] = Complex64::default();
+            buf_b[i] = Complex64::default();
+        }
+
         let forward_plan = C2CPlan::new(
             &[rows, cols], &mut buf_a, &mut buf_b, Sign::Forward, Flag::MEASURE
         ).unwrap();
 
-        // NB backward: trap -> slm
         let backward_plan = C2CPlan::new(
             &[rows, cols], &mut buf_b, &mut buf_a, Sign::Backward, Flag::MEASURE
         ).unwrap();
 
-        println!("Allocated memory buffers and plans for (fftw) holography.");
-
         HolographyEngine { rows, cols, buf_a, buf_b, forward_plan, backward_plan }
     }
 
-    fn initialize_random_phase(amplitude: &Array2<f64>) -> Array2<Complex64> {
+    fn initialize_in_place_random(buf: &mut [Complex64], illumination: &Array2<f64>) {
         let mut rng = rand::thread_rng();
-        let mut field = Array2::<Complex64>::zeros(amplitude.raw_dim());
+        let ill_slice = illumination.as_slice().unwrap();
 
-        Zip::from(&mut field)
-            .and(amplitude)
-            .for_each(|f, &amp| {
-                let phase = rng.gen_range(0.0..(2.0 * std::f64::consts::PI));
-                *f = Complex64::from_polar(amp, phase);
-            });
-
-        field
-    }
-
-    fn constrain_amplitude(complex_field: &mut AlignedVec<Complex64>, target_amplitude: &Array2<f64>) {
-        let amplitude_slice = target_amplitude.as_slice().unwrap();
-
-        for (c, &target_amp) in complex_field.iter_mut().zip(amplitude_slice.iter()) {
-            let phase = c.arg(); 
-            *c = Complex64::from_polar(target_amp, phase);
+        for (c, &amp) in buf.iter_mut().zip(ill_slice.iter()) {
+            let phase = rng.gen_range(0.0..(2.0 * std::f64::consts::PI));
+            *c = Complex64::from_polar(amp, phase);
         }
     }
 
-    fn extract_phase(complex_field: &Array2<Complex64>) -> Array2<f64> {
-        let mut phase_map = Array2::<f64>::zeros(complex_field.raw_dim());
-
-        Zip::from(&mut phase_map)
-            .and(complex_field)
-            .for_each(|p, c| {
-                // NB (0 to 2π)
-                *p = c.arg() + std::f64::consts::PI;
-        });
-
-        phase_map
+    #[inline(always)]
+    fn constrain_amplitude(complex_buf: &mut [Complex64], target_amp_slice: &[f64]) {
+        for (c, &target_amp) in complex_buf.iter_mut().zip(target_amp_slice.iter()) {
+            let norm = c.norm();
+            if norm > 1e-15 {
+                let scale = target_amp / norm;
+                c.re *= scale;
+                c.im *= scale;
+            } else {
+                *c = Complex64::new(target_amp, 0.0);
+            }
+        }
     }
 
     pub fn gerchberg_saxton_slm_phase(
@@ -79,24 +69,28 @@ impl HolographyEngine {
         slm_illumination: &Array2<f64>, 
         iterations: usize
     ) -> Array2<f64> {
-        let mut current_slm_field = Self::initialize_random_phase(slm_illumination);
+        let target_amp_slice = target_amplitude.as_slice().unwrap();
+        let slm_ill_slice = slm_illumination.as_slice().unwrap();
+
+        Self::initialize_in_place_random(&mut self.buf_a, slm_illumination);
 
         for _ in 0..iterations {
-            self.buf_a.copy_from_slice(current_slm_field.as_slice().unwrap());
-
             self.forward_plan.c2c(&mut self.buf_a, &mut self.buf_b).unwrap();
 
-            Self::constrain_amplitude(&mut self.buf_b, target_amplitude);
+            Self::constrain_amplitude(&mut self.buf_b, target_amp_slice);
 
             self.backward_plan.c2c(&mut self.buf_b, &mut self.buf_a).unwrap();
 
-            Self::constrain_amplitude(&mut self.buf_a, slm_illumination);
-            
-            current_slm_field.as_slice_mut().unwrap().copy_from_slice(&self.buf_a);
+            Self::constrain_amplitude(&mut self.buf_a, slm_ill_slice);
         }
 
-        println!("Completed Gerchberg-Saxton");
+        let mut phase_map = Array2::<f64>::zeros((self.rows, self.cols));
+        let phase_slice = phase_map.as_slice_mut().unwrap();
 
-        Self::extract_phase(&current_slm_field)
+        for (p, c) in phase_slice.iter_mut().zip(self.buf_a.iter()) {
+            *p = c.arg() + std::f64::consts::PI;
+        }
+
+        phase_map
     }
 }
