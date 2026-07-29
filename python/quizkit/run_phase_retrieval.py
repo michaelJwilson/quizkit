37cd4e50e29160b7ef545a8d38d4688a98d9c290
Michael J. Wilson <michaeljwilson@princeton.edu>
2026-07-29 13:14:12 -0400

    fix

--- python/quizkit/run_phase_retrieval.py
+++ python/quizkit/run_phase_retrieval.py
@@ -847,7 +847,7 @@ class HologramExperimentSolver:
 
 
 def run_phase_retrieval():
-    method = "GD"
+    method = "GS"
     solver_backend = "jax"  # {"slm_suite", "jax"}
     num_random_seeds = 1
     slm_shape = (1200, 1920)  # (height, width) in pixels,
